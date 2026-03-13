42d9c6e835df90d6c6a64b9bb3ad58983e755790
unknown <iragenaegide205@gmail.com>
2026-03-13 22:53:54 +0200

    rate analysis

--- src/pages/dashboard/RateAnalysis.tsx
+++ src/pages/dashboard/RateAnalysis.tsx
@@ -279,7 +279,7 @@ const RateAnalysis: React.FC = () => {
         </div>
       </div>
 
-      {/* Search Bar */}
+     
       <div className="relative max-w-md">
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
         <Input
